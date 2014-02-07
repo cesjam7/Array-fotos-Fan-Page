69fed10d4aad42ba6567dcb25b83561228f2362e
César Jefferson Aquino Maximiliano <cesar7492@gmail.com>
2014-02-07 17:31:07 -0500

    Create images.php
    Modificar el 000000000000

---
+++ images.php
@@ -0,0 +1,7 @@
+<?php
+   define('PAGE_ID', '000000000000000');
+   define('APP_ID','');
+   define('APP_SECRET','');
+   include("phpcUrl.php");
+   $face = new FacePageAlbum(PAGE_ID, $_GET['aid'], $_GET['aurl'], APP_ID, APP_SECRET);
+?>
